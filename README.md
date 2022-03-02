3b1019a1517bea005eb11aedcb43ed3f44e2a1ad
This Dev <thisdev2021@gmail.com>
2022-03-02 10:55:50 -0300

    Primeiro commit, calculadora finalizada

--- README.md
+++ README.md
@@ -1,46 +1,30 @@
-# Getting Started with Create React App
+# Calculadora de IMC TS
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Calculadora básica de imc criada com React + Typescript.
 
-## Available Scripts
+Este projeto foi desenvolvido no curso B7WEB.
 
-In the project directory, you can run:
+### Features
 
-### `npm start`
+- [x] Calculo de IMC
+- [x] Responsividade
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### Pré requisitos
+Antes de iniciar você precisa ter o [Node](https://nodejs.org/en/) e o [Typescript](https://www.typescriptlang.org/) instalado na sua máquina. É bom também ter um editor de código como [VSCode](https://code.visualstudio.com/).
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+### Instalação
+- `npm install`
 
-### `npm test`
+### Executar
+- `npm start`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Tecnologias
 
-### `npm run build`
+Neste projeto foram usadas as seguintes tecnologias
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- [React JS](https://pt-br.reactjs.org/)
+- [Javascript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+- [Typescript](https://www.typescriptlang.org/)
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+<hr/>
+Criado com dedicação por <a href="https://github.com/d8web/" target="_blank">Daniel</a>.
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import styles from "./App.module.css"
+import poweredImage from "./assets/powered.png"
+import leftArrowImage from "./assets/leftarrow.png"
+import { GridItem } from "./components/GridItem"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { levels, calculateImc, Level } from "./helpers/imc"
+
+const App = () => {
+
+    const [ heightField, setheightField ] = useState<number>(0)
+    const [ weightField, setWeightField ] = useState<number>(0)
+    const [ toShow, setToShow ] = useState<Level | null>(null)
+
+    const handleCalculateButton = () => {
+
+        if(heightField && weightField) {
+            setToShow(calculateImc(heightField, weightField))
+        } else {
+            alert("Preencha todos os campos corretamente!")
+        }
+        
+    }
+
+    const handleBackButton = () => {
+        setToShow(null)
+        setheightField(0)
+        setWeightField(0)
+    }
+
+    return (
+        <div className={styles.main}>
+            <header>
+                <div className={styles.headerContainer}>
+                    <img src={poweredImage} width={150} alt="Logo" />
+                </div>
+            </header>
+            <div className={styles.container}>
+                <div className={styles.leftSide}>
+                    <h1>Calcule o seu IMC.</h1>
+                    <p>IMC é a sigla para Índice de Massa Corporal,que é um cálculo que serve para avaliar se a pessoa está dentro do seu peso ideal em relação à altura.</p>
+
+                    <input
+                        type="number"
+                        placeholder="Digite sua altura. Ex: 1.5 (em metros)"
+                        value={heightField > 0 ? heightField : ""}
+                        onChange={e => setheightField(parseFloat(e.target.value))}
+                        disabled={toShow ? true : false}
+                    />
+
+                    <input
+                        type="number"
+                        placeholder="Digite seu peso. Ex: 57.8 (em Kg)"
+                        value={weightField > 0 ? weightField : ""}
+                        onChange={e => setWeightField(parseFloat(e.target.value))}
+                        disabled={toShow ? true : false}
+                    />
+
+                    <button
+                        onClick={handleCalculateButton}
+                        disabled={toShow ? true : false}
+                    >Calcular</button>
+
+                </div>
+                <div className={styles.rightSide}>
+                    {!toShow &&
+                        <div className={styles.grid}>
+                            {levels.map((item, key) => (
+                                <GridItem key={key} item={item} />
+                            ))}
+                        </div>
+                    }
+                    {toShow &&
+                        <div className={styles.rightBig}>
+                            <div className={styles.rightArrow} onClick={handleBackButton}>
+                                <img src={leftArrowImage} alt="" width={26} />
+                            </div>
+                            <GridItem item={toShow} />
+                        </div>
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
